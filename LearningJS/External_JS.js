779efea942009a5a4c63034598dbe78fec0d116f
Yuri Potawsky <potawsky.yuri@gmail.com>
2021-10-15 16:23:10 -0400

    Operators

---
+++ LearningJS/External_JS.js
@@ -0,0 +1,35 @@
+//alert("Hello Yuri")
+
+let message = "Hello";
+//alert(message);
+
+
+//Use of Constant Variables
+const myBirthday = "7/5/1984"
+
+//alert(myBirthday);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+//alert(color); // #FF7F00
+
+console.log(`The coloring is ${color}`);
+
+let testOne;
+console.log(typeof(COLOR_RED));
+//console.log(testOne);
+//prompt("Hello Yuri", 37);
+
+//
+// let value = true;
+// alert(typeof value); // boolean
+//
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+
+console.log(15%6);
